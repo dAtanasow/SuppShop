528ed717caccf4341de9f6a588cfeaab91d33df3
Denis Atanasov <denisatanasov76@gmail.com>
2025-03-27 14:31:44 +0200

    create Header, install router and add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
 import Home from "./components/home/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <Header />
+      <main id="main-content" className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
