c58c8840d2263399a43cdbf7fafd5fb7807a17a2
Denis Atanasov <denisatanasov76@gmail.com>
2025-03-27 20:47:05 +0200

    make private and public header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router";
+import { useAuthContext } from "../context/AuthContext";
 
 export default function Header() {
+  const { userId, isAuthenticate } = useAuthContext();
+
   return (
     <header className="bg-white shadow">
       <nav className="flex items-center justify-between p-2 text-lg">
@@ -24,34 +27,45 @@ export default function Header() {
             </Link>
           </li>
 
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-              />
-            </svg>
-          </li>
+          {userId && (
+            <li>
+              <Link to={`/cart/${userId}`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                  />
+                </svg>
+              </Link>
+            </li>
+          )}
         </ul>
 
         <div className="flex w-[20%] justify-end gap-10 pr-5">
-          <Link to="/create">Add Product</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/logout">Logout</Link>
-          <Link to="/login" className="hover:text-gray-700">
-            Login
-          </Link>
-          <Link to="/register" className="hover:text-gray-700">
-            Register
-          </Link>
+          {isAuthenticate ? (
+            <>
+              <Link to="/create">Add Product</Link>
+              <Link to="/profile">Profile</Link>
+              <Link to="/logout">Logout</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="hover:text-gray-700">
+                Login
+              </Link>
+              <Link to="/register" className="hover:text-gray-700">
+                Register
+              </Link>
+            </>
+          )}
         </div>
       </nav>
     </header>
