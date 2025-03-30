dc171594a5ab72af03a9c9d275e609b9dc412c8d
Denis Atanasov <denisatanasov76@gmail.com>
2025-03-30 10:05:49 +0300

    create products-api and useProducts

---
+++ client/src/hooks/useProducts.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import productsApi from "../аpi/products-api";
+
+export function useGetAllProducts(category, brand) {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+            setError(null);
+            const result = await productsApi.getAll(category, brand);
+            if (result && result.length > 0) {
+                setProducts(result);
+            } else {
+                setProducts([]);
+                setError("No items found");
+            }
+            setLoading(false);
+        })()
+    }, [category, brand])
+
+    return [products, loading, error];
+}
---
+++ client/src/аpi/products-api.js
@@ -0,0 +1,21 @@
+import * as request from './requester'
+
+const BASE_URL = 'http://localhost:3000/api/catalog';
+
+const getAll = async (category, brand) => {
+    const query = new URLSearchParams();
+    if (category) {
+        query.append('category', category);
+    }
+    if (brand) {
+        query.append('brand', brand);
+    }
+    const result = await request.get(`${BASE_URL}?${query.toString()}`);
+    return result;
+}
+
+const productsApi = {
+    getAll,
+}
+
+export default productsApi;
